d8f4ad68b2c83da3ee765a8568a708d6bc7f8ae6
NavdiwalaVatsal <navdiwalavatsal.vn@gmail.com>
2022-04-20 16:07:49 +0530

    ui

---
+++ lib/screen/events_view.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class EventsView extends StatefulWidget {
+  const EventsView({ Key? key }) : super(key: key);
+
+  @override
+  State<EventsView> createState() => _EventsViewState();
+}
+
+class _EventsViewState extends State<EventsView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      
+    );
+  }
+}
